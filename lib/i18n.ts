d615796495ae2e8cd9c0fc45a3768cf32f28ac58
berkecodestr <berkebozkurtcodes@gmail.com>
2026-06-20 20:01:05 +0300

    Add translations for Turkish and English languages

---
+++ lib/i18n.ts
@@ -0,0 +1,16 @@
+export const translations = {
+  tr: {
+    ST: 'SNT', LW: 'SLK', RW: 'SĞK',
+    GK: 'KL', CB: 'STP', LB: 'SLB', RB: 'SĞB',
+    CM: 'MO', CAM: 'MOO', CDM: 'MDO',
+    LM: 'SLO', RM: 'SĞO', LWB: 'SLKB', RWB: 'SĞKB',
+    CF: 'MF'
+  },
+  en: {
+    ST: 'ST', LW: 'LW', RW: 'RW',
+    GK: 'GK', CB: 'CB', LB: 'LB', RB: 'RB',
+    CM: 'CM', CAM: 'CAM', CDM: 'CDM',
+    LM: 'LM', RM: 'RM', LWB: 'LWB', RWB: 'RWB',
+    CF: 'CF'
+  }
+};
